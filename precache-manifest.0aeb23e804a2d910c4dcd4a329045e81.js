0d670b6a4fcf23f5f51084ee7c3a24cb6ae35994
Michael W Lu <michaelwenlu@gmail.com>
2020-02-19 20:37:48 -0500

    Updates

---
+++ precache-manifest.0aeb23e804a2d910c4dcd4a329045e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659f253643045ee020db16eaa1ccc904",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "34ee9e1b57aa5a5420c0",
+    "url": "/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "6edcdf994bf5c4113157",
+    "url": "/react-tutorial/static/js/2.1ceb358c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-tutorial/static/js/2.1ceb358c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34ee9e1b57aa5a5420c0",
+    "url": "/react-tutorial/static/js/main.d8047253.chunk.js"
+  },
+  {
+    "revision": "d1a4ad59d776d93e8cfb",
+    "url": "/react-tutorial/static/js/runtime-main.4b063f4c.js"
+  }
+]);
